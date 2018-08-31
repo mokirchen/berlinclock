f454ac1dc16cc7eaabb9a55c9ae55f47763217e9
Moritz Kirchenberger <moritz.kirchenberger@computacenter.com>
2018-08-31 14:14:31 +0200

    Added TimeConverterExtensions Class to extend string builder: better performance since we make use of string builders representation of a "mutable" string instead of creating new strings on each concatenation

--- Classes/TimeConverter.cs
+++ Classes/TimeConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BerlinClock.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,101 +16,17 @@ namespace BerlinClock
             int hours = int.Parse(timeParts[0]);
             int minutes = int.Parse(timeParts[1]);
             int seconds = int.Parse(timeParts[2]);
-        
+
             //calculate each row independently and append to string
             StringBuilder berlinClockBuilder = new StringBuilder();
-            berlinClockBuilder.Append(CalculateSecondsLamp(seconds));
-            berlinClockBuilder.Append("\n" + CalculateFirstHourRowLamps(hours));
-            berlinClockBuilder.Append("\n" + CalculateSecondHourRowLamps(hours));
-            berlinClockBuilder.Append("\n" + CalculateFirstMinuteRowLamps(minutes));
-            berlinClockBuilder.Append("\n" + CalculateSecondMinuteRowLamps(minutes));
+
+            berlinClockBuilder.CalculateSecondsLamp(seconds);
+            berlinClockBuilder.CalculateFirstHourRowLamps(hours);
+            berlinClockBuilder.CalculateSecondHourRowLamps(hours);
+            berlinClockBuilder.CalculateFirstMinuteRowLamps(minutes);
+            berlinClockBuilder.CalculateSecondMinuteRowLamps(minutes);
 
             return berlinClockBuilder.ToString();
         }
-
-        private string CalculateSecondsLamp(int seconds)
-        {
-            return seconds % 2 == 0 ? "Y" : "O";
-        }
-
-        private string CalculateFirstHourRowLamps(int hours)
-        {
-            string row = string.Empty;
-            int timesDivisibleByFive = hours / 5;
-            for (int i = 1; i <= 4; i++)
-            {
-                if (i <= timesDivisibleByFive)
-                {
-                    row += "R";
-                }
-                else
-                {
-                    row += "O";
-                }
-            }
-            return row;
-        }
-
-        private string CalculateSecondHourRowLamps(int hours)
-        {
-            int divisionRemainder = hours % 5;
-            string row = string.Empty;
-            for (int i = 1; i <= 4; i++)
-            {
-                if (i <= divisionRemainder)
-                {
-                    row += "R";
-                }
-                else
-                {
-                    row += "O";
-                }
-            }
-            return row;
-        }
-
-        private string CalculateFirstMinuteRowLamps(int minutes)
-        {
-            string row = string.Empty;
-            int timesDivisibleByFive = minutes / 5;
-            for (int i = 1; i <= 11; i++)
-            {
-                if (i <= timesDivisibleByFive)
-                {
-                    //check if quarter lamp is reached
-                    if (i % 3 == 0)
-                    {
-                        row += "R";
-                    }
-                    else
-                    {
-                        row += "Y";
-                    }
-                }
-                else
-                {
-                    row += "O";
-                }
-            }
-            return row;
-        }
-
-        private string CalculateSecondMinuteRowLamps(int minutes)
-        {
-            string row = string.Empty;
-            int divisionRemainder = minutes % 5;
-            for (int i = 1; i <= 4; i++)
-            {
-                if (i <= divisionRemainder)
-                {
-                    row += "Y";
-                }
-                else
-                {
-                    row += "O";
-                }
-            }
-            return row;
-        }
-    }
+    }   
 }
---
+++ Classes/TimeConverterExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerlinClock.Classes
+{
+    public static class TimeConverterExtensions
+    {
+        public static void CalculateSecondsLamp(this StringBuilder sb, int seconds) => sb.Append(seconds % 2 == 0 ? "Y" : "O");
+
+        public static void CalculateFirstHourRowLamps(this StringBuilder sb, int hours)
+        {
+            sb.Append("\n");
+            int timesDivisibleByFive = hours / 5;
+            for (int i = 1; i <= 4; i++)
+            {
+                if (i <= timesDivisibleByFive)
+                {
+                    sb.Append("R");
+                }
+                else
+                {
+                    sb.Append("O");
+                }
+            }
+        }
+
+        public static void CalculateSecondHourRowLamps(this StringBuilder sb, int hours)
+        {
+            sb.Append("\n");
+            int divisionRemainder = hours % 5;
+            for (int i = 1; i <= 4; i++)
+            {
+                if (i <= divisionRemainder)
+                {
+                    sb.Append("R");
+                }
+                else
+                {
+                    sb.Append("O");
+                }
+            }
+        }
+
+        public static void CalculateFirstMinuteRowLamps(this StringBuilder sb, int minutes)
+        {
+            sb.Append("\n");
+            int timesDivisibleByFive = minutes / 5;
+            for (int i = 1; i <= 11; i++)
+            {
+                if (i <= timesDivisibleByFive)
+                {
+                    //check if quarter lamp is reached
+                    if (i % 3 == 0)
+                    {
+                        sb.Append("R");
+                    }
+                    else
+                    {
+                        sb.Append("Y");
+                    }
+                }
+                else
+                {
+                    sb.Append("O");
+                }
+            }
+        }
+
+        public static void CalculateSecondMinuteRowLamps(this StringBuilder sb, int minutes)
+        {
+            sb.Append("\n");
+            int divisionRemainder = minutes % 5;
+            for (int i = 1; i <= 4; i++)
+            {
+                if (i <= divisionRemainder)
+                {
+                    sb.Append("Y");
+                }
+                else
+                {
+                    sb.Append("O");
+                }
+            }
+        }
+    }
+}
